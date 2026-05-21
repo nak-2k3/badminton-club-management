2c23049f72183a6b7e2ac4159a58aea678baa29f
NAK-203 <nak.203.job@gmail.com>
2026-05-22 00:14:26 +0700

    Khoi tao

---
+++ src/main/java/com/badmintonclub/clubmanagement/ClubmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.badmintonclub.clubmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/badmintonclub/clubmanagement/ClubmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.badmintonclub.clubmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
