8bd68875ac691d7b6192faef196b357befe94880
NAK-203 <nak.203.job@gmail.com>
2026-05-22 21:55:37 +0700

    tao User Entity va Repository

---
+++ src/main/java/com/badmintonclub/clubmanagement/entity/User.java
@@ -0,0 +1,31 @@
+package com.badmintonclub.clubmanagement.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullName;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String phone;
+
+    private String role;
+
+    private String level;
+
+    private boolean enabled = true;
+}
